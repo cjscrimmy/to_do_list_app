d94fad3a7efcb6e55ebec720dd98153d996b6b74
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-11 18:14:26 +0100

    can display a pre-created list on the screen

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el:"#app"
+        el:"#app",
+        data: {
+            items: [
+                {title: "Tidy Bedroom", priority: "high"},
+                {title: "Wash Dishes", priority: "low"},
+                {title: "Do Shopping", priority: "high"}
+            ],
+            // newItem:""
+        }
     });
 });
